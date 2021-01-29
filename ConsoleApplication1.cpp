c72147235e15b3f85ff548103bb5d870728e0705
Дмитрий Нусс <nuss_dmitriy@mail.ru>
2021-01-29 20:48:35 +0400

    New

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,10 +2,8 @@
 
 using namespace std;
 
-int main()
+void oneArray()
 {
-	setlocale(LC_ALL, "Russian");
-	
 	int array[5];
 
 	cout << "Enter element massiva" << endl;
@@ -26,4 +24,64 @@ int main()
 		sum += array[i];
 		cout << " " << sum << " ";
 	}	
+}
+
+void twoArray()
+{
+    int n, i, * a, f;
+    cout << "Введите количество элементов массива: ";
+    cin >> n;
+    f = 0;
+    a = new int[n];
+    for (i = 0; i < n; i++)
+    {
+        cout << "Введите элемент массива: ";
+        cin >> a[i];
+    }
+    for (i = 1; i < n; i++)
+    {
+        if (a[i - 1] - a[i] > 0)
+        {
+            f = 1;
+        }
+        else
+        {
+            f = -1;
+        }
+    }
+    if (f == -1)
+    {
+        if (a[i - 1] - a[i] < 0)
+        {
+            f = 2;
+        }
+        else
+            f = -1;
+    }
+    if (f == 1)
+    {
+        cout << "Последовательность убывает \n";
+    }
+    if (f == 2)
+    {
+        cout << "Последовательность возрастает \n";
+    }
+    if (f == -1) cout << "Последовательность не возрастает и не убывает";
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	//oneArray();
+
+	twoArray();
+
+	//treeArray();
+
+	//fourArray();
+
+	//fiveArray();
+
+	return 0;
 }
