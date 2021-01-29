94ec58599799ea031fefe9db4fb4040655ee5b3d
Дмитрий Нусс <nuss_dmitriy@mail.ru>
2021-01-29 20:33:53 +0400

    New

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 
 using namespace std;
@@ -6,21 +5,25 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	
+	int array[5];
+
+	cout << "Enter element massiva" << endl;
 	int sum = 0;
-	cout << "массив: " << endl;
 
-	// объявление и инициализация одномерного массива
-	int array[4] = { 1, 2, 3, 4 };
-
-	cout << "Index" << "\t\t" << "Element massiva" << endl;
-
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 5; i++)
 	{
-		// вывод на экран индекса ячейки массива, а затем содержимого этой ячейки, в нашем случае - это целое число
-		cout << "array[" << i << "]" << "\t\t" << array[i] << endl;
+		cin >> array[i];
 	}
-	for (int i = 0; i < 4; i++) {
+	//cout << "array = { ";
+	
+	for (int i = 0; i < 5; i++)
+	{
+		cout << " " << array[i] << " ";
+	}
+	cout << '\n';
+	for (int i = 0; i < 5; i++) {
 		sum += array[i];
-		cout << "array[" << i << "]" << "\t\t" << sum << endl;
-	}
+		cout << " " << sum << " ";
+	}	
 }
